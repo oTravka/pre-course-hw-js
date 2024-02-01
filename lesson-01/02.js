5e2ffd628f9c21458e0a659ff88814c0c2a5a18a
oTravka <153914867+oTravka@users.noreply.github.com>
2024-02-01 12:48:20 +0300

    Update 02.js
    Программа генерирует рандомное число и проверяет чётное оно или нет.

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,6 @@
-// code
+let number1 = Math.floor(Math.random() * 100);
+if (number1 % 2 === 0) {
+    console.log(number1, 'Это чётное число');
+}else{
+    console.log(number1, 'Это нечётное число');
+};
