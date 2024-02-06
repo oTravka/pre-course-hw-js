b5da868db2e7a80793118b13c163eafaaf951f41
oTravka <153914867+oTravka@users.noreply.github.com>
2024-02-06 17:22:53 +0300

    Create 06.js

---
+++ lesson-02/06.js
@@ -0,0 +1,14 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+let clone = Object.assign({}, passportWithAddress);
+clone.address = {
+    city: 'Bobryisk'
+};
+console.log(passportWithAddress.address.city)
+console.log(clone.address.city)
