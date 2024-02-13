37e99618ffd6f0ddd0ce4ec6aed75c52060112a9
oTravka <153914867+oTravka@users.noreply.github.com>
2024-02-13 14:38:45 +0300

    Update 01.js

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,11 @@
-// code
+const colors =["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function checkColor(colors) {
+    return (colors === "черный") || (colors === "красный")|| (colors === "желтый");
+}
+
+function createColorString() {
+    return colors.filter(checkColor).join('-')
+}
+
+console.log(createColorString());
